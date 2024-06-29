558a31efcaf85e8320ab347ed681340192d7d876
Hagotits <162283387+Hagotits@users.noreply.github.com>
2024-06-29 17:07:30 +0900

    /write - 게시글 작성 페이지 구현

--- src/components/Write.js
+++ src/components/Write.js
@@ -15,8 +15,8 @@ const Write = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const onSubmit = async ({ name, price, peple, date, time, place, explan }) => {
-    const body = { name, quan, price, peple, date, time, place, explan };
+  const onSubmit = async ({ name, price, peple, date, place, explan }) => {
+    const body = { name, quan, price, peple, date, place, explan };
     // try {
     //   await dispatch(registerUser(body));
     //   navigate("/login");
@@ -40,9 +40,6 @@ const Write = () => {
   const userDate = {
     required: "필수 필드입니다.",
   };
-  const userTime = {
-    required: "필수 필드입니다.",
-  };
   const userPlace = {
     required: "필수 필드입니다.",
   };
@@ -53,13 +50,14 @@ const Write = () => {
   return (
     <div>
       <div className="WritePage">
-        <div className="subdiv">
-          <div className="title"></div>
+        <div className="Writesubdiv">
+          <div className="Writetitle">글 작성</div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="contentTitle">
-              <div className="inputTitle">상품 이미지</div>
-              <div className="inputWrite">
-                <label className="img">이미지 등록</label>
+            
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">상품 이미지</div>
+              <div className="WriteinputWrite">
+                <label className="img"></label>
                 <input
                   type="file"
                   className="file"
@@ -68,114 +66,122 @@ const Write = () => {
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">상품명</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  placeholder="상품명을 입력해주세요"
-                  className="input"
-                  {...register("name", userName)}
-                />
-                {errors?.name && (
-                  <div className="errMessage">
-                    <span>{errors.name.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">상품명</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                  <input
+                    type="text"
+                    placeholder=" 상품명을 입력해주세요"
+                    className="input"
+                    {...register("name", userName)}
+                  />
+                  {errors?.name && (
+                    <div className="errMessage">
+                      <span>{errors.name.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">가격</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  placeholder="가격을 입력해주세요"
-                  className="input"
-                  {...register("price", userPrice)}
-                />
-                {errors?.price && (
-                  <div className="errMessage">
-                    <span>{errors.price.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">가격</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                  <input
+                    type="text"
+                    placeholder=" 가격을 입력해주세요"
+                    className="input"
+                    {...register("price", userPrice)}
+                  />
+                  {errors?.price && (
+                    <div className="errMessage">
+                      <span>{errors.price.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">인원</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  placeholder="명"
-                  className="input"
-                  {...register("peple", userPeple)}
-                />
-                {errors?.peple && (
-                  <div className="errMessage">
-                    <span>{errors.peple.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">인원</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                <select className="input" {...register("peple", userPeple)}>
+                    <option value={""}> 나눠가지길 희망하는 인원수를 선택해주세요</option>
+                    <option value={"A"}>1</option>
+                    <option value={"B"}>2</option>
+                    <option value={"C"}>3</option>
+                    <option value={"D"}>4</option>
+                  </select>
+                  {errors?.peple && (
+                    <div className="errMessage">
+                      <span>{errors.peple.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">수령날짜 / 시간</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  className="input"
-                  {...register("date", userDate)}
-                />
-                {errors?.date && (
-                  <div className="errMessage">
-                    <span>{errors.date.message}</span>
-                  </div>
-                )}
-                <input
-                  type="text"
-                  className="input"
-                  {...register("time", userTime)}
-                />
-                {errors?.time && (
-                  <div className="errMessage">
-                    <span>{errors.time.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">수령날짜 / 시간</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                  <input
+                    type="datetime-local"
+                    className="input"
+                    {...register("date", userDate)}
+                  />
+                  {errors?.date && (
+                    <div className="errMessage">
+                      <span>{errors.date.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">수령장소</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  placeholder="선택"
-                  className="input"
-                  {...register("place", userPlace)}
-                />
-                {errors?.place && (
-                  <div className="errMessage">
-                    <span>{errors.place.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">수령장소</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                  <select className="input" {...register("place", userPlace)}>
+                    <option value={""}> 선택해주세요</option>
+                    <option value={"A"}>신촌 세븐 앞</option>
+                    <option value={"B"}>신촌 짱돌 앞</option>
+                    <option value={"C"}>단월 농협 앞</option>
+                    <option value={"D"}>모시래 세븐 앞</option>
+                    <option value={"D"}>모시래 기숙사 여동 앞 </option>
+                    <option value={"E"}>모시래 기숙사 남동 앞 </option>
+                    <option value={"F"}>해오름 기숙사 여동 앞 </option>
+                    <option value={"G"}>해오름 기숙사 남동 앞 </option>
+                  </select>
+                  {errors?.place && (
+                    <div className="errMessage">
+                      <span>{errors.place.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="contentTitle">
-              <div className="inputTitle">설명</div>
-              <div className="inputWrite">
-                <input
-                  type="text"
-                  placeholder="설명을 입력해주세요. 식품인 경우 유통기한을 꼭! 작성해주세요."
-                  className="input"
-                  {...register("explan", userExplan)}
-                />
-                {errors?.explan && (
-                  <div className="errMessage">
-                    <span>{errors.explan.message}</span>
-                  </div>
-                )}
+            <div className="WritecontentDiv">
+              <div className="WriteinputTitle">설명</div>
+              <div className="WriteinputWrite">
+                <div className="Writeexplandiv">
+                  <input
+                    type="text"
+                    placeholder=" 설명을 입력해주세요. 식품인 경우 유통기한을 꼭! 작성해주세요."
+                    className="Writeexplaninput"
+                    {...register("explan", userExplan)}
+                  />
+                  {errors?.explan && (
+                    <div className="errMessage">
+                      <span>{errors.explan.message}</span>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
