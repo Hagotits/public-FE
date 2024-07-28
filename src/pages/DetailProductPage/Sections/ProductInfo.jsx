25297798d1b048dbc0dd1857188376eea5530840
Hagotits <162283387+Hagotits@users.noreply.github.com>
2024-07-28 18:05:59 +0900

    userName 수정

--- src/pages/DetailProductPage/Sections/ProductInfo.jsx
+++ src/pages/DetailProductPage/Sections/ProductInfo.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../../redux/thunkFunctions";
+import dayjs from "dayjs";
+
 
 const ProductInfo = ({ product }) => {
   const dispatch = useDispatch();
@@ -37,12 +39,69 @@ const ProductInfo = ({ product }) => {
 
     return `${days}일 ${hours}시간 ${minutes}분 ${seconds}초`;
   };
+  // console.log(product)
 
   return (
     <div>
       <p>상품 정보</p>
+        <div
+          id="글쓴 회원 정보"
+          className="w-full h-[100px] border-b border-gray-500 relative flex py-[10px]"
+        >
+          <div
+            id="프로필 이미지"
+            className="w-[80px] h-full rounded-full border-2 border-[#c1c1c1]"
+          ></div>
+          <div className="h-full flex flex-col justify-center items-start ml-[20px]">
+            <div id="닉네임" className="mb-[5px]">
+              {product.userName}
+            </div>
+            <div id="거래 장소">{product.places}</div>
+          </div>
+        </div>
 
-      <div>
+        <div>
+          <div
+            id="제목"
+            className="w-full h-[50px] text-[25px] font-normal mt-[30px] mb-[10px]"
+          >
+                {product.title}
+              </div>
+              <div
+                id="가격"
+                className="w-full h-[40px] text-[20px] font-bold mb-[10px]"
+              >
+                {product.price}원
+              </div>
+              <div id="설명" className="text-[16px]">
+                {product.content}
+              </div>
+            </div>
+
+            <div className="relative flex justify-end flex-row items-end mt-[50px]">
+              <div id="회색글씨" className="flex flex-col items-end mr-[10px]">
+                <div
+                  id="남은 인원"
+                  className="text-[12px] text-[rgb(182, 182, 182)]"
+                >
+                  {product.attend - 1}명 남음
+                </div>
+                <div
+                  id="남은 시간"
+                  className="text-[12px] text-[rgb(182, 182, 182)] mt-[3px]"
+                >
+                  {dayjs(product.remainTime).format("YYYY-MM-DD HH:mm:ss")}
+                </div>
+              </div>
+              <button
+                id="참여 버튼"
+                className="w-[200px] h-10 text-[14px] font-semibold bg-purple-800 rounded-md text-white hover:bg-puple-400"
+                onClick={handleClick}
+              >
+                {Math.floor(product.price / product.attend)}원으로 참여하기
+              </button>
+            </div>
+      {/* <div>
         <div>{product.title}</div>
         <div>
           <span>인원: </span>
@@ -71,7 +130,7 @@ const ProductInfo = ({ product }) => {
         <button onClick={handleClick}>
           {Math.floor(product.price / product.attend)}원으로 참여하기
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/pages/DetailProductPage/index.jsx
+++ src/pages/DetailProductPage/index.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../utils/axios";
 import ProductImage from "./Sections/ProductImage";
-import dayjs from "dayjs";
 import ProductInfo from "./Sections/ProductInfo";
 
 const DetailProductPage = () => {
@@ -38,62 +37,10 @@ const DetailProductPage = () => {
           >
             <ProductImage product={product} />
           </div>
-          <div
-            id="글쓴 회원 정보"
-            className="w-full h-[100px] border-b border-gray-500 relative flex py-[10px]"
-          >
-            <div
-              id="프로필 이미지"
-              className="w-[80px] h-full rounded-full border-2 border-[#c1c1c1]"
-            ></div>
-            <div className="h-full flex flex-col justify-center items-start ml-[20px]">
-              <div id="닉네임" className="mb-[5px]">
-                {product.userName}
-              </div>
-              <div id="거래 장소">{product.places}</div>
-            </div>
-          </div>
-
           <div>
-            <div
-              id="제목"
-              className="w-full h-[50px] text-[25px] font-normal mt-[30px] mb-[10px]"
-            >
-              {product.title}
-            </div>
-            <div
-              id="가격"
-              className="w-full h-[40px] text-[20px] font-bold mb-[10px]"
-            >
-              {product.price}원
-            </div>
-            <div id="설명" className="text-[16px]">
-              {product.content}
-            </div>
+            <ProductInfo product={product} />
           </div>
-
-          <div className="relative flex justify-end flex-row items-end mt-[50px]">
-            <div id="회색글씨" className="flex flex-col items-end mr-[10px]">
-              <div
-                id="남은 인원"
-                className="text-[12px] text-[rgb(182, 182, 182)]"
-              >
-                {product.attend - 1}명 남음
-              </div>
-              <div
-                id="남은 시간"
-                className="text-[12px] text-[rgb(182, 182, 182)] mt-[3px]"
-              >
-                {dayjs(product.receptTime).format("YYYY-MM-DD HH:mm:ss")}
-              </div>
-            </div>
-            <button
-              id="참여 버튼"
-              className="w-[200px] h-10 text-[14px] font-semibold bg-purple-800 rounded-md text-white hover:bg-puple-400"
-            >
-              {Math.floor(product.price / product.attend)}원으로 참여하기
-            </button>
-          </div>
+          
         </div>
       )}
     </div>
