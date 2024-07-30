73d70ee3e5f576b68612bbc4967e802bd4d2e782
Hagotits <162283387+Hagotits@users.noreply.github.com>
2024-07-30 10:46:49 +0900

    게시글 찜 구현 중

--- src/pages/MainPage/Sections/CardItem.jsx
+++ src/pages/MainPage/Sections/CardItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ImageSlider from "../../../components/ImageSlider";
 import dayjs from "dayjs";
+import { IoHeartOutline } from "react-icons/io5";
 
 const CardItem = ({ product }) => {
   return (
@@ -9,22 +10,31 @@ const CardItem = ({ product }) => {
       <Link to={`/products/${product.id}`}>
         <div>
           <div className="image h-48 bg-gray-100 overflow-hidden">
+            <IoHeartOutline
+              style={{
+                width: "15%",
+                height: "15%",
+                color:"gray",
+              }}
+            />
             <ImageSlider images={product.images || []} />
           </div>
           <p className="p-1">{product.title}</p>
           <p className="p-1 text-xs text-gray-500">{product.price}원</p>
           <p className="p-1">{product.place}</p>
-          <p className="p-1 text-xs text-gray-500">
-            {product.attend - 1}명 남음
-          </p>
-          <p className="p-1 text-xs text-gray-500">
-            {dayjs(product.receptTime).format("YYYY-MM-DD HH:mm:ss")}
-          </p>
         </div>
       </Link>
 
-      <div className="w-full mt-2.5">
-        <button className="w-[50%] h-10 text-sm font-semibold bg-[#2B0585] rounded-md text-white hover:bg-[#8186CB]">
+      <div className="flex w-full mt-2.5">
+        <div className="flex-1 flex-col justify-end text-right">
+          <p className="p-1 text-[10px] text-gray-500">
+            {product.attend - 1}명 남음
+          </p>
+          <p className="p-1 text-[10px] text-gray-500">
+            {dayjs(product.receptTime).format("YYYY-MM-DD HH:mm")}
+          </p>
+        </div>
+        <button className="w-[55%] h-10 text-xs font-semibold bg-[#2B0585] rounded-lg text-white hover:bg-[#8186CB]">
           {product.price / product.attend}원으로 참여하기
         </button>
       </div>
