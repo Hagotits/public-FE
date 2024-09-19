834849607998a05251c7ec96a20b64287f62e42a
Hagotits <162283387+Hagotits@users.noreply.github.com>
2024-09-19 09:27:31 +0900

    디테일 페이지 더 둘러보기 누르면 메인으로 이동

--- src/pages/DetailProductPage/Sections/PopularityPost.jsx
+++ src/pages/DetailProductPage/Sections/PopularityPost.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import MainPage from '../../MainPage/index';
+import CardItem from '../../MainPage/Sections/CardItem';
+import { useNavigate } from 'react-router-dom';
 
 const PopularityPost = () => {
-
+  const navigate = useNavigate();
+  
   return (
     <div>
-      <div className="flex justify-start py-[40px]">
+      <div className="flex py-[40px] justify-between">
         <h3 className="text-lg font-semibold">인기 게시물</h3>
-        <button className="text-[#2B0585] hover:text-bold right-0">더 구경하기</button>
+        <button
+          className="text-[#2B0585] hover:text-bold right-0"
+          onClick={() => navigate("/")}>더 구경하기</button>
       </div>
-      <div>여긴 찜 갯수 n개 이상인 게시물 띄우기(하면 될 듯?)</div>
+      <div>여긴 찜 갯수 n개 이상인 게시물 띄우기(하면 될 듯?)
+        <div>{CardItem}</div>
+      </div>
     </div>
   );
 };
