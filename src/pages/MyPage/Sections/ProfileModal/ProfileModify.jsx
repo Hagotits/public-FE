52abbe6094597a020520e5c0243378b08e04e8f6
Hagotits <162283387+Hagotits@users.noreply.github.com>
2024-09-19 15:09:42 +0900

    프로필 수정 모달 병합 완

--- src/pages/MyPage/Sections/ProfileModal/ProfileModify.jsx
+++ src/pages/MyPage/Sections/ProfileModal/ProfileModify.jsx
@@ -20,7 +20,7 @@ const ProfileModify = ({
   );
   const [name, setName] = useState(null); // 이름 상태 초기값을 null로 설정
   const fileInput = useRef(null);
-  const fileInputRef = useRef(null); // 파일 입력 참조
+  // const fileInputRef = useRef(null); // 파일 입력 참조
 
   // 이미지 미리보기 처리 함수
   const handleImageChange = (e) => {
@@ -74,101 +74,118 @@ const ProfileModify = ({
 
   return (
     modifyModal && (
-      <div className="fixed left-0 inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-        <div className="relative bg-white w-[500px] h-[550px] p-16 rounded-lg shadow-xl flex flex-col">
-          {/* 닫기 버튼 */}
-          <button
-            className="absolute top-4 right-4 text-gray-600 hover:text-gray-900"
-            onClick={() => setModifyModal(false)}
-          >
-            ✕
-          </button>
+      <div>
+        <div className="fixed left-0 inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+          <div className="relative bg-white w-[500px] h-[600px] p-16 rounded-lg shadow-xl flex flex-col">
+            {/* 닫기 버튼 */}
+            <button
+              className="absolute top-4 right-4 text-gray-600 hover:text-gray-900"
+              onClick={() => setModifyModal(false)}
+            >
+              ✕
+            </button>
 
-          {/* 프로필 이미지 미리보기 */}
-          <div className="flex justify-center mb-4">
-            <img
-              src={imagePreview}
-              alt="Profile Preview"
-              className="w-32 h-32 rounded-full object-cover"
-              onClick={() => fileInput.current.click()}
-            />
-          </div>
+            {/* 프로필 수정 폼 */}
+            <form
+              onSubmit={handleSubmit(profileModify)}
+              className="flex-1 flex flex-col"
+            >
+              {/* 프로필 이미지 업로드 */}
+              <div className="flex flex-col items-center">
+                <label className="text-[22px] font-semibold text-gray-800 mb-4">
+                  프로필 수정
+                </label>
 
-          {/* 프로필 수정 폼 */}
-          <form onSubmit={handleSubmit(profileModify)}>
-            {/* 프로필 이미지 업로드 */}
-            <div className="flex flex-col items-center mb-6">
-              <label className="text-lg font-semibold text-gray-800 mb-4">
-                프로필 사진 변경
-              </label>
-              <input
-                type="file"
-                accept="image/*"
-                ref={fileInput}
-                onChange={handleImageChange}
-                className="hidden"
-              />
-              {errors.avatar && (
-                <p className="text-sm text-red-500">{errors.avatar.message}</p>
-              )}
-            </div>
-
-            {/* 사용자 이름 변경 */}
-            <div className="grid grid-cols-[100px_1fr] items-center border-b-[1px] border-gray-300 py-3">
-              <label className="text-base font-medium text-gray-800">
-                이름
-              </label>
-              <input
-                className="w-full text-sm font-normal text-gray-800 p-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="새로운 사용자 이름을 입력하세요"
-                onChange={handleNameChange}
-              />
-              {errors.name && (
-                <p className="text-sm text-red-500 mt-2">
-                  {errors.name.message}
-                </p>
-              )}
-            </div>
-
-            {/* 비밀번호 변경 */}
-            <div className="grid grid-cols-[100px_1fr] items-center border-b-[1px] border-gray-300 py-3">
-              <label className="text-base font-medium text-gray-800">
-                비밀번호
-              </label>
-              <button
-                type="button"
-                className="ml-auto w-[80px] text-gray-700 px-4 py-2 rounded-[20px] hover:bg-gray-200"
-                onClick={() => setPasswordModal(true)}
+                {/* 프로필 이미지 미리보기 */}
+                <div className="relative mb-4">
+                  <img
+                    src={imagePreview}
+                    alt="Profile Preview"
+                    className="w-32 h-32 rounded-full object-cover"
+                    onClick={() => fileInput.current.click()}
+                  />
+                  <div
+                  className="absolute w-10 h-10 bottom-0 right-0 text-gray-500 bg-gray-200 cursor-pointer grid place-items-center rounded-full"
+                  onClick={() => fileInput.current.click()} // 카메라 아이콘 클릭하면 파일 선택하도록
+                  >
+                    <FaCamera style={{ fontSize: "1.4rem" }} />
+                  </div>
+                </div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  ref={fileInput}
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+                {errors.avatar && (
+                  <p className="text-sm text-red-500">{errors.avatar.message}</p>
+                )}
+              </div>
+              <div
+                className="flex justify-end mb-7 text-[15px] text-gray-300 cursor-pointer"
+                // onClick={} 클릭하면 현재 올라와있는 이미지 삭제시키기
               >
-                변경
-              </button>
-            </div>
+                프로필 이미지 삭제
+              </div>
 
-            {/* 비밀번호 변경 모달 */}
-            {passwordModal && (
-              <PassWordChange
-                passwordModal={passwordModal}
-                setPasswordModal={setPasswordModal}
-              />
-            )}
+              {/* 사용자 이름 변경 */}
+              <div className="grid grid-cols-[100px_1fr] items-center border-b-[1px] border-gray-300 py-3">
+                <label className="text-base font-medium text-gray-800">
+                  이름
+                </label>
+                <input
+                  className="w-full text-sm font-normal text-gray-800 p-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+                  placeholder="새로운 사용자 이름을 입력하세요"
+                  onChange={handleNameChange}
+                />
+                {errors.name && (
+                  <p className="text-sm text-red-500 mt-2">
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
 
+              {/* 비밀번호 변경 */}
+              <div className="grid grid-cols-[100px_1fr] items-center border-b-[1px] border-gray-300 py-3">
+                <label className="text-base font-medium text-gray-800">
+                  비밀번호
+                </label>
+                <button
+                  type="button"
+                  className="ml-auto w-[80px] text-gray-700 px-4 py-2 rounded-[20px] hover:bg-gray-200"
+                  onClick={() => setPasswordModal(true)}
+                >
+                  변경
+                </button>
+              </div>
+
+              {/* 비밀번호 변경 모달 */}
+              {passwordModal && (
+                <PassWordChange
+                  passwordModal={passwordModal}
+                  setPasswordModal={setPasswordModal}
+                />
+              )}
+
+            </form>
             {/* 버튼 섹션 */}
             <div className="flex justify-end space-x-3 mt-6">
-              <button
-                type="submit"
-                className="bg-[#2B0585] text-white px-4 py-2 rounded-md hover:bg-blue-700"
-              >
-                수정하기
-              </button>
-              <button
-                type="button"
-                className="bg-gray-300 text-black py-2 px-4 rounded-md hover:bg-gray-400"
-                onClick={() => setModifyModal(false)}
-              >
-                취소
-              </button>
-            </div>
-          </form>
+                <button
+                  type="submit"
+                  className="bg-[#2B0585] text-white px-4 py-2 rounded-md hover:bg-blue-700"
+                >
+                  수정하기
+                </button>
+                <button
+                  type="button"
+                  className="bg-gray-300 text-black py-2 px-4 rounded-md hover:bg-gray-400"
+                  onClick={() => setModifyModal(false)}
+                >
+                  취소
+                </button>
+              </div>
+          </div>
         </div>
       </div>
     )
